4244a5c20b2da3d7a4aac26c8aeff679f688577d
reza shomali nezhad <r.shomalinezhad@gmail.com>
2021-01-11 16:40:45 +0100

    clean App.js

--- src/App.js
+++ src/App.js
@@ -21,27 +21,6 @@ const useStyles = makeStyles((theme) => ({
       marginRight: "auto",
     },
   },
-  paper: {
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(3),
-    padding: theme.spacing(2),
-    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-      marginTop: theme.spacing(6),
-      marginBottom: theme.spacing(6),
-      padding: theme.spacing(3),
-    },
-  },
-  stepper: {
-    padding: theme.spacing(3, 0, 5),
-  },
-  buttons: {
-    display: "flex",
-    justifyContent: "flex-end",
-  },
-  button: {
-    marginTop: theme.spacing(3),
-    marginLeft: theme.spacing(1),
-  },
 }));
 
 function App() {
@@ -62,7 +41,7 @@ function App() {
       <AppBar color="default" className={classes.appBar}>
         <Toolbar>
           <Typography variant="h6" color="inherit" noWrap>
-            SIXT
+            SIXT - frontend coding task
           </Typography>
         </Toolbar>
       </AppBar>
