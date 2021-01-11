2c6ffc9b6c3f68de165531058920ee4cac323162
reza shomali nezhad <r.shomalinezhad@gmail.com>
2021-01-11 16:42:22 +0100

    add App.js test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+// We're using our own custom render function and not RTL's render
+// our custom utils also re-export everything from RTL
+// so we can import fireEvent and screen here as well
+import { render, screen } from "./test-utils";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("Renders the connected app with initialState", () => {
+  const initialState = {
+    loading: true,
+    offers: [],
+    error: "",
+  };
+  render(<App />, { initialState: initialState });
+  expect(screen.getByText(/SIXT/i)).toBeInTheDocument();
 });
